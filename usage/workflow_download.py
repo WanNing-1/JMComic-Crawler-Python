d242301bbda299a39f5f3bda5ea2d49f8ac4c16a
WanNing-1 <3103583742@qq.com>
2026-05-30 10:45:09 +0800

    Update workflow_download.py

--- usage/workflow_download.py
+++ usage/workflow_download.py
@@ -2,6 +2,8 @@ from jmcomic import *
 from jmcomic.cl import JmcomicUI
 from jmcomic import download_album, Feature
 
+download_album('1441962', extra=Feature.export_pdf)
+
 # 下方填入你要下载的本子的id，一行一个，每行的首尾可以有空白字符
 jm_albums = '''
 JM1441962
@@ -52,7 +54,6 @@ def main():
     option = get_option()
     helper.run(option)
     option.call_all_plugin('after_download')
-    download_album('123', option, extra=Feature.export_pdf)
 
 def get_option():
     # 读取 option 配置文件
